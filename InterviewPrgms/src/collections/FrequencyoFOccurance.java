f38225154f068dddaf4e0ec20b57a00da9b073db
raufmca <raufmca@gmail.com>
2020-03-23 18:14:05 +0530

    Interview1

---
+++ InterviewPrgms/src/collections/FrequencyoFOccurance.java
@@ -0,0 +1,24 @@
+package collections;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class FrequencyoFOccurance {
+
+	public static void main(String[] args) {
+		
+		int ar[] = {1,2,3,1,2,3,5,6,7,8,9,5,6,7};
+		
+		HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+		
+		for ( int i: ar) {
+			if( map.containsKey(i))
+				map.put(i, map.get(i)+1);
+			else
+				map.put(i, 1);
+		}
+		
+		System.out.println("Arrays list -> "+ Arrays.toString(ar));
+		System.out.println("Result -> "+ map);
+	}
+
+}
