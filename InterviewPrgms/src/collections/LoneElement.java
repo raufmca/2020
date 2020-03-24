317c8c1a5847db22a60b12fb4f1cae97ecda39d0
raufmca <raufmca@gmail.com>
2020-03-24 14:50:11 +0530

    Program 2

--- InterviewPrgms/src/collections/LoneElement.java
+++ InterviewPrgms/src/collections/LoneElement.java
@@ -1,12 +1,36 @@
 package collections;
 
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class LoneElement {
 
 	public static void main(String[] args) {
 
-		int[] ar = {1,2,2,3,3,4,4,5,6,7,5,6,7};
+		int[] ar = {1,2,2,3,3,4,4,5,6,7,5,6,7,8,7,3,2,4,6};
 		
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i: ar) {
+			if(map.containsKey(i))
+				map.put(i, map.get(i)+1);
+			else
+				map.put(i, 1);
+		}
 
+		
+		System.out.println("Map details -> "+ map);
+		for( Entry<Integer, Integer> entry : map.entrySet())
+			if( entry.getValue() <= 1)
+				System.out.println("Lone Elent is -> " + entry.getKey());
+				
+		
+		
+		
+		
 	}
+	
+	
+	
 
 }
---
+++ Test/src/TestMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class TestMap {
+	
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+		
+		hmap.put(1,"Abdulrouf");
+		hmap.put(2,"Shamsunnisa");
+		hmap.put(3,"Alhaam");
+		hmap.put(4, "Aqdas");
+		hmap.put(5, "Bijapur");
+		
+		System.out.println("Value at 4 - > " + hmap.get(4));
+		
+		System.out.println("Value at 5 -> " + hmap.get(5));
+	}
+
+}
